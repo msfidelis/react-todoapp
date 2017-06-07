4ebf7ede2e717423ea7e6d35faabd45a92504d9f
Matheus Fidelis <msfidelis01@gmail.com>
2017-06-06 22:10:06 -0300

    Cors e Dependencias

---
+++ backend/src/config/cors.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*') 
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+}
