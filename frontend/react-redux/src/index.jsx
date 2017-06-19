8635fb5217f750355dfe0d40830ca25df441bbc3
Matheus Fidelis <msfidelis@outlook.com>
2017-06-19 13:27:21 -0300

    Install redux

--- frontend/react-redux/src/index.jsx
+++ frontend/react-redux/src/index.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import App from './main/app'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const reducers = combineReducers({
+    field : () => ({
+        value : "opa"
+    })
+})
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>
+, document.getElementById('app'))
